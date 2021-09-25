91556418cd3bf7df4aa76ab38b76eaf3e18e7b69
Sean Kevin Farrell <seanykevinfarrell@gmail.com>
2021-09-24 22:55:19 -0600

    Cell + leantween.

--- AOE/Assets/Cell.cs
+++ AOE/Assets/Cell.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
+    public bool on;
+    public GameObject _obj;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        on = false;
+        _obj.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Switch()
     {
-        
+        if (on)
+        {
+            on = false;
+            StartCoroutine("TurnOff");
+            _obj.SetActive(false);
+        }
+        else
+        {
+            on = true;
+            _obj.SetActive(true);
+            StartCoroutine("TurnOn");
+
+        }
     }
+
+
+    IEnumerator TurnOn()
+    {
+
+        _obj.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, speed);
+        yield return new WaitForSecondsRealtime(speed);
+    }
+
+    IEnumerator TurnOff()
+    {
+        _obj.transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, speed);
+        yield return new WaitForSecondsRealtime(speed);
+    }
+
 }
